4fc8d2d486f74422693c3fb0adf70f9b81ee4b6f
Rizky Dhani <rizkydhni03@gmail.com>
2026-03-07 19:22:22 +0700

    refactor: simplify Setting model with direct attributes

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -12,41 +12,76 @@ class Setting extends Model
     protected $fillable = [
         'key',
         'value',
+        'venue_name',
+        'venue_address',
+        'venue_latitude',
+        'venue_longitude',
+        'venue_detection_radius',
     ];
 
     protected $casts = [
         'value' => 'string',
+        'venue_latitude' => 'float',
+        'venue_longitude' => 'float',
+        'venue_detection_radius' => 'integer',
     ];
-
-    public static function get(string $key, mixed $default = null): mixed
-    {
-        $setting = static::where('key', $key)->first();
-
-        if (! $setting) {
-            return $default;
-        }
-
-        return $setting->value;
-    }
-
-    public static function set(string $key, mixed $value): void
-    {
-        static::updateOrCreate(
-            ['key' => $key],
-            ['value' => $value]
-        );
-    }
 
     public static function getVenueCoordinates(): array
     {
+        $setting = static::first();
+
         return [
-            'lat' => (float) static::get('venue_latitude', -6.2147245),
-            'lng' => (float) static::get('venue_longitude', 106.8073332),
+            'lat' => $setting?->venue_latitude ?? -6.2147245,
+            'lng' => $setting?->venue_longitude ?? 106.8073332,
         ];
     }
 
     public static function getVenueRadius(): int
     {
-        return (int) static::get('venue_detection_radius', 500);
+        $setting = static::first();
+
+        return $setting?->venue_detection_radius ?? 500;
+    }
+
+    public static function getVenueName(): string
+    {
+        $setting = static::first();
+
+        return $setting?->venue_name ?? 'Jakarta International Expo';
+    }
+
+    public static function getVenueAddress(): string
+    {
+        $setting = static::first();
+
+        return $setting?->venue_address ?? 'Jl. Expo Kemayoran, Jakarta Pusat';
+    }
+
+    public static function setVenueInfo(
+        ?string $name = null,
+        ?string $address = null,
+        ?float $latitude = null,
+        ?float $longitude = null,
+        ?int $radius = null
+    ): void {
+        $setting = static::first() ?? new static;
+
+        if ($name !== null) {
+            $setting->venue_name = $name;
+        }
+        if ($address !== null) {
+            $setting->venue_address = $address;
+        }
+        if ($latitude !== null) {
+            $setting->venue_latitude = $latitude;
+        }
+        if ($longitude !== null) {
+            $setting->venue_longitude = $longitude;
+        }
+        if ($radius !== null) {
+            $setting->venue_detection_radius = $radius;
+        }
+
+        $setting->save();
     }
 }
