9c7e1744e0b161140c8eed0cda43afc5ae173b0b
Rizky Dhani <rizkydhni03@gmail.com>
2026-03-12 17:53:34 +0700

    feat: autofocus search bar when opening Select Country dropdown

--- resources/views/livewire/seminar-registration.blade.php
+++ resources/views/livewire/seminar-registration.blade.php
@@ -146,7 +146,7 @@
                         <label class="block text-sm font-medium text-gray-700 mb-1">{{ __('seminar.country') }} *</label>
                         {{-- Searchable Country Dropdown --}}
                         <div class="relative" @click.away="countryOpen = false">
-                            <button type="button" @click="countryOpen = !countryOpen" 
+                            <button type="button" @click="countryOpen = !countryOpen; if (countryOpen) $nextTick(() => $refs.countrySearch.focus())" 
                                 class="w-full px-4 py-2 border border-gray-300 rounded-lg text-left bg-white focus:ring-2 focus:ring-blue-500 focus:border-blue-500 flex justify-between items-center">
                                 <span>
                                     {{ $country_id ? $countries->firstWhere('id', $country_id)->name : __('seminar.select_country') }}
@@ -159,7 +159,7 @@
                             <div x-show="countryOpen" x-cloak 
                                 class="absolute z-50 w-full mt-1 bg-white border border-gray-300 rounded-lg shadow-lg max-h-80 overflow-y-auto">
                                 <div class="p-2 border-b border-gray-200">
-                                    <input type="text" x-model="countrySearch" 
+                                    <input type="text" x-model="countrySearch" x-ref="countrySearch"
                                         placeholder="{{ __('seminar.search_country') }}"
                                         class="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                                         @keydown.escape="countryOpen = false">
