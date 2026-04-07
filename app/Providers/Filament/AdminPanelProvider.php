d91a12de995c5045bb1766393e56eb90f19934de
Rizky Dhani <rizkydhni03@gmail.com>
2026-04-07 15:23:35 +0700

    style: apply Pint formatting to imports

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -25,8 +25,6 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 
-use function Filament\Support\original_request;
-
 class AdminPanelProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
