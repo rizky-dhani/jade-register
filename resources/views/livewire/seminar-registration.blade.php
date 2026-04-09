74d2c6eebeff30cbe622615b36ee2cf5ceacd2a6
Rizky Dhani <rizkydhni03@gmail.com>
2026-04-09 10:09:47 +0700

    ui: update hands-on notice text and add selection instruction
    - Change 'Workshop Hands-On' to 'Sesi Hands-On' in notice
    - Add blue instruction text to select hands-on sessions separately below package
    - Update English and Indonesian translation keys

--- resources/views/livewire/seminar-registration.blade.php
+++ resources/views/livewire/seminar-registration.blade.php
@@ -289,6 +289,12 @@
                         </svg>
                         {{ __('seminar.package_excludes_hands_on') }}
                     </p>
+                    <p class="text-sm text-blue-700 bg-blue-50 border border-blue-200 rounded-lg p-3 mb-3">
+                        <svg class="inline-block w-4 h-4 mr-1 -mt-0.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                        </svg>
+                        {{ __('seminar.select_hands_on_below_package') }}
+                    </p>
 
                     @php
                     $tiers = $availableTiers ?? [];
