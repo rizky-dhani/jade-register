70752628ba270eef672f01a6e990ab0fb37ff21f
Rizky Dhani <rizkydhni03@gmail.com>
2026-04-13 11:33:27 +0700

    fix: correct translation namespace for seminar registration report description
    - Change from 'report.seminar_registration_description' to 'seminar.seminar_registration_description'
    - Translation keys are in seminar.php, not report.php

--- resources/views/filament/pages/seminar-registration-report.blade.php
+++ resources/views/filament/pages/seminar-registration-report.blade.php
@@ -1,5 +1,5 @@
 <x-filament-panels::page>
     <p class="text-gray-600 dark:text-gray-400 mb-4">
-        {{ __('report.seminar_registration_description') }}
+        {{ __('seminar.seminar_registration_description') }}
     </p>
 </x-filament-panels::page>
