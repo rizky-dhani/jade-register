9ebd4229604b679277648c2372acdadb76920d4a
Rizky Dhani <rizkydhni03@gmail.com>
2026-04-13 13:30:05 +0700

    feat: split seminar registration export into Local and International participant sheets
    - Refactor SeminarRegistrationExport to use WithMultipleSheets
    - Sheet 1: Local Participants (Indonesia country or null country_id)
    - Sheet 2: International Participants (all other countries)
    - Extract LocalParticipantsSheet and InternationalParticipantsSheet classes
    - Reuse headings and mapping logic via protected methods

--- app/Exports/SeminarRegistrationExport.php
+++ app/Exports/SeminarRegistrationExport.php
@@ -3,19 +3,49 @@
 namespace App\Exports;
 
 use App\Models\SeminarRegistration;
+use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 
-class SeminarRegistrationExport implements FromCollection, ShouldAutoSize, WithHeadings, WithMapping
+class SeminarRegistrationExport implements WithMultipleSheets
 {
+    public function sheets(): array
+    {
+        return [
+            new LocalParticipantsSheet,
+            new InternationalParticipantsSheet,
+        ];
+    }
+}
+
+class LocalParticipantsSheet implements FromCollection, ShouldAutoSize, WithHeadings, WithMapping
+{
+    use Exportable;
+
     public function collection()
     {
-        return SeminarRegistration::with('country', 'user')->get();
+        return SeminarRegistration::with('country', 'user')
+            ->whereHas('country', function ($query) {
+                $query->where('name', 'Indonesia');
+            })
+            ->orWhereNull('country_id')
+            ->get();
     }
 
     public function headings(): array
+    {
+        return $this->getHeadings();
+    }
+
+    public function map($registration): array
+    {
+        return $this->mapData($registration);
+    }
+
+    protected function getHeadings(): array
     {
         return [
             'ID',
@@ -48,7 +78,7 @@ class SeminarRegistrationExport implements FromCollection, ShouldAutoSize, WithH
         ];
     }
 
-    public function map($registration): array
+    protected function mapData($registration): array
     {
         return [
             $registration->id,
@@ -79,5 +109,29 @@ class SeminarRegistrationExport implements FromCollection, ShouldAutoSize, WithH
             $registration->created_at?->format('Y-m-d H:i:s'),
             $registration->updated_at?->format('Y-m-d H:i:s'),
         ];
+    }
+}
+
+class InternationalParticipantsSheet implements FromCollection, ShouldAutoSize, WithHeadings, WithMapping
+{
+    use Exportable;
+
+    public function collection()
+    {
+        return SeminarRegistration::with('country', 'user')
+            ->whereHas('country', function ($query) {
+                $query->where('name', '!=', 'Indonesia');
+            })
+            ->get();
+    }
+
+    public function headings(): array
+    {
+        return (new LocalParticipantsSheet)->getHeadings();
+    }
+
+    public function map($registration): array
+    {
+        return (new LocalParticipantsSheet)->mapData($registration);
     }
 }
