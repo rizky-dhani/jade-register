6006d16d81d8de6d939e078a5c6a3b437f953651
Rizky Dhani <rizkydhni03@gmail.com>
2026-04-27 14:55:11 +0700

    fix(hands-on): redirect create and edit pages to index after save

--- app/Filament/Resources/HandsOns/Pages/CreateHandsOn.php
+++ app/Filament/Resources/HandsOns/Pages/CreateHandsOn.php
@@ -8,4 +8,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateHandsOn extends CreateRecord
 {
     protected static string $resource = HandsOnResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
