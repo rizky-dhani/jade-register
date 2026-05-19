fb38120b7da9c80ad9b3c081121b690527bb8b43
Rizky Dhani <rizkydhni03@gmail.com>
2026-05-19 10:21:51 +0700

    feat: add queue:work scheduler for shared hosting (cron watchdog replacement)

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('queue:work', [
+    '--queue' => 'high,default',
+    '--sleep' => 3,
+    '--tries' => 3,
+    '--max-time' => 3600,
+    '--stop-when-empty' => true,
+])->everyMinute()->withoutOverlapping();
