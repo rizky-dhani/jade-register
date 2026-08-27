b6835f0c7f8a65a82a5f79d83245801d8343e41b
Rizky Dhani <rizkydhni03@gmail.com>
2026-08-27 15:41:46 +0700

    Remove deadline date text from seminar registration form

--- resources/views/livewire/seminar-registration.blade.php
+++ resources/views/livewire/seminar-registration.blade.php
@@ -431,7 +431,6 @@
                 <h2 class="text-xl font-semibold text-gray-800 mb-4">{{ __('seminar.registration_package') }}</h2>
 
                 <div class="md:col-span-2">
-                    <p class="text-xs text-gray-500 mb-3">{{ __('seminar.valid_until') }}</p>
                     <p class="text-base font-medium text-amber-800 bg-amber-50 border-2 border-amber-300 rounded-lg p-3 mb-3">
                         <svg class="inline-block w-5 h-5 mr-1.5 -mt-0.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
