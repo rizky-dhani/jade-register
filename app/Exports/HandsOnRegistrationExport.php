d9bde3b602d8e3d7ac5b07754c53b6ce4d1bb37a
Rizky Dhani <rizkydhni03@gmail.com>
2026-09-10 10:02:22 +0700

    Center align totals table text on summary sheet

--- app/Exports/HandsOnRegistrationExport.php
+++ app/Exports/HandsOnRegistrationExport.php
@@ -13,6 +13,7 @@ use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 use Maatwebsite\Excel\Concerns\WithStyles;
 use Maatwebsite\Excel\Concerns\WithTitle;
 use Maatwebsite\Excel\Events\AfterSheet;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
 class HandsOnRegistrationExport implements WithMultipleSheets
@@ -193,6 +194,7 @@ class HandsOnSummarySheet extends BaseHandsOnSheet implements WithEvents
             ->get();
 
         $row = $sheet->getHighestRow() + 2;
+        $firstRow = $row;
 
         $sheet->fromArray(['HO Code', 'Paid', 'Pending', 'Total'], null, 'A'.$row, true);
         $sheet->getStyle('A'.$row.':D'.$row)->getFont()->setBold(true);
@@ -205,6 +207,10 @@ class HandsOnSummarySheet extends BaseHandsOnSheet implements WithEvents
             $sheet->fromArray([$session->ho_code, $paid, $pending, $paid + $pending], null, 'A'.$row, true);
             $row++;
         }
+
+        $sheet->getStyle('A'.$firstRow.':D'.($row - 1))
+            ->getAlignment()
+            ->setHorizontal(Alignment::HORIZONTAL_CENTER);
     }
 }
 
